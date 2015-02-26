f061db35db862ef437d00120d05ed266b22f4c59
adwisatya <a.dwisaty4@yahoo.com>
2015-02-26 19:49:08 +0700

    x

--- engine/connect.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$db = "arc_url_shortener"
-	$user = "root";
-	$pass = "";
-	$host = "localhost";
-	mysql_connect($host, $user, $pass);
-	mysql_select_db($db);
-?>
--- engine/process.js
+++ engine/process.js
@@ -1,3 +1,23 @@
-function insertURL(url){
+function insertURL(){
+	var title = document.getElementById('title');
+	var link = document.getElementById('link');
 	
+}
+
+function GetXmlHttpObject() {
+	var xmlhttp=null;
+	try {
+		// Firefox, Opera 8.0+, Safari
+		xmlhttp=new XMLHttpRequest();
+	}
+	catch (e) {
+		// Internet Explorer
+		try {
+			xmlhttp=new ActiveXObject("Msxml2.XMLHTTP");
+		}
+		catch (e) {
+			xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+		}
+	}
+	return xmlhttp;
 }
